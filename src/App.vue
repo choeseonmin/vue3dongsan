b8dac1242620ae4105d086a00b7aa92599a79960
sllavv <129834569+choeseonmin@users.noreply.github.com>
2023-09-04 14:56:02 +0900

    허위매물신고 버튼 만듬

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  <div class="menu">
+    <a v-for="(a,i)  in 메뉴들" :key="i">{{ a }}</a> 
+  </div>
+
   <div>
     <h4>{{ products[0] }}</h4>
     <p>50만원</p> 
+    <button @click="신고수[0]++">허위매물신고</button> 
+    <span>신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
-    <p>가격은 아무거나</p>
+    <p>60만원</p> 
+    <button @click="신고수[1]++">허위매물신고</button> 
+    <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
     <h4>{{ products[2] }}</h4>
-    <p>가격은 아무거나</p>
+    <p>70만원</p> 
+    <button @click="신고수[2]++">허위매물신고</button>
+    <span>신고수 : {{ 신고수[2] }}</span>
   </div>
-</template>   
+
+  </template>   
  
 <script>  
+
 
 export default {
   name: 'App',
   data(){ 
     return {
+      신고수 : [0,0,0],
+      메뉴들 : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
+  
   components: {
 
   }
@@ -36,6 +51,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background:  darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
 </style>
