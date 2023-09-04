42e1b24b7b7fcc48a5f1d555ab9ddac6bcfcbca5
sllavv <129834569+choeseonmin@users.noreply.github.com>
2023-09-04 16:17:48 +0900

    이름클릭시 모달창열림, 닫기버튼 생성

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <div class="white-bg">
         <h4>상세페이지</h4>
         <p>상세페이지 내용</p>
+        <button @click="모달창열림 = false">닫기</button>
       </div>
   </div>
 
@@ -13,24 +14,24 @@
 
   <div>
     <img src="./assets/room0.jpg" class="room-img">
-    <h4>{{ products[0] }}</h4>
+    <h4 @click="모달창열림 = true">{{ products[0] }}</h4>
     <p>50만원</p> 
     <button @click="신고수[0]++">허위매물신고</button> 
-    <span>신고수 : {{ 신고수[0] }}</span>
+    <span> 신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
     <img src="./assets/room1.jpg" class="room-img">
-    <h4>{{ products[1] }}</h4>
+    <h4 @click="모달창열림 = true">{{ products[1] }}</h4>
     <p>60만원</p> 
     <button @click="신고수[1]++">허위매물신고</button> 
-    <span>신고수 : {{ 신고수[1] }}</span>
+    <span> 신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
     <img src="./assets/room2.jpg" class="room-img">
-    <h4>{{ products[2] }}</h4>
+    <h4 @click="모달창열림 = true">{{ products[2] }}</h4>
     <p>70만원</p> 
     <button @click="신고수[2]++">허위매물신고</button>
-    <span>신고수 : {{ 신고수[2] }}</span>
+    <span> 신고수 : {{ 신고수[2] }}</span>
   </div>
 
   </template>   
@@ -42,7 +43,7 @@ export default {
   name: 'App',
   data(){ 
     return {
-      모달창열림 : true,
+      모달창열림 : false,
       신고수 : [0,0,0],
       메뉴들 : ['Home', 'Shop', 'About'],
       products : ['평거동원룸', '가좌동원룸', '칠암동원룸'],
